977150894fde13226722301439d190dd5e48dc9d
Howell <ltghc1990@gmail.com>
2021-06-09 21:01:23 -0400

    added desktop styles

--- components/FaqsAccordion.js
+++ components/FaqsAccordion.js
@@ -14,34 +14,31 @@ const FaqsAccordion = () => {
     }
   }
 
-  const bgGradient = {backgroundImage: "linear-gradient(120deg, hsl(273, 75%, 66%), hsl(240, 73%, 65%)"}
+  const bgGradient = {backgroundImage: "linear-gradient(180deg, hsl(273, 75%, 66%), hsl(240, 73%, 65%)"}
 
   const accordion = data.map((item, index)=>{
     return(
-      <div key={index} className="my-1 border-b-2 w-11/12 mx-auto">
-        <div className="border-b-2 border-gray-300"> 
-        <div className="flex justify-between" onClick={()=>onClickHandler(index)}>
-          <p className={`${selected === index ? "font-bold" : "text-md"} pb-3`}>{item.question}</p>
-          <p>{selected === index ? "-" : "+"}</p>
+        <div key={index} className="border-b-2 border-gray-300 pt-4"> 
+          <div className="flex justify-between" onClick={()=>onClickHandler(index)}>
+            <p className={`${selected === index ? "font-bold" : "text-md"} pb-3`}>{item.question}</p>
+            <p>{selected === index ? "-" : "+"}</p>
+          </div>
+          <p className={`${selected === index ? "block" : "hidden"}`}>{item.answer}</p>
         </div>
-         
-        <p className={`${selected === index ? "block" : "hidden"}`}>{item.answer}</p>
-        </div>
-      </div>
     )
   })
 
   return (
-    <div style={bgGradient} className=" pt-40 pb-20">
-      <div className="bg-gray-200 max-w-sm border-2 mx-auto rounded-3xl shadow-2xl pt-28 pb-8 md:max-w-xl">
-        <div className ="relative"> 
-          <img className=" -mt-52 absolute left-1/2 transform -translate-x-1/2" src="\images\faq-accordion\illustration-woman-online-mobile.svg"></img>
-          <img className="absolute -mt-46 left-1/2 transform -translate-x-1/2 -top-24" src="\images\faq-accordion\bg-pattern-mobile.svg"/>
-          <p className="text-center font-bold text-4xl mb-8 tracking-wider">FAQ</p>
+    <div style={bgGradient} className="h-screen pt-40 lg:pt-20">
+      <div className="bg-gray-200 border-2 max-w-sm mx-auto rounded-3xl shadow-2xl pt-28 pb-8 sm:max-w-xl lg:max-w-screen-md lg:flex lg:py-12 lg:px-8 xl:max-w-screen-lg">
+        <div className ="relative lg:w-1/2"> 
+          <img className="-mt-52 absolute z-50 left-1/2 transform -translate-x-1/2 lg:mt-0 lg:w-11/12 lg:top-1/2 lg:-translate-y-1/2 lg:left-1/3" src="\images\faq-accordion\illustration-woman-online-mobile.svg"></img>
+          <img className="absolute -mt-46 left-1/2 transform -translate-x-1/2 -top-24 lg:w-11/12 lg:top-1/2 lg:-translate-y-1/6 lg:left-1/3" src="\images\faq-accordion\bg-pattern-mobile.svg"/>
         </div>
-         
-        {accordion}
-
+        <div className="my-1 border-b-2 w-11/12 mx-auto lg:w-1/2">
+          <p className="text-center font-bold text-4xl mb-8 tracking-wider lg:text-left">FAQ</p>
+          {accordion}
+        </div>
       </div>
     </div>
   )
